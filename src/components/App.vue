9e603cf5ff7ebe27c3fa1fce26171673025f0cce
Allan Alves <allanalvesmail@gmail.com>
2018-07-29 15:20:36 -0300

    Alteração de versões do package json para resolução de conflitos e criação do component para criação das semanas e datas do calendário

---
+++ src/components/App.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div v-for='week in weeks' :key='week'>
+            Week
+            <div v-for="day in week" :key="day">
+                {{day}}
+            </div>
+        </div>
+        </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      month: 7,
+      year: 2018
+    };
+  },
+  computed: {
+    days() {
+      let days = [];
+      let currentDay = this.$moment(`${this.year}-${this.month}-1`, "YYYY-M-D");
+
+      do {
+        days.push(currentDay);
+        currentDay = this.$moment(currentDay).add(1, "days");
+      } while (currentDay.month() + 1 == this.month);
+
+      currentDay = this.$moment(days[0]);
+
+      const SUNDAY = 0;
+      const MONDAY = 1;
+
+      if (currentDay.day != MONDAY) {
+        do {
+          currentDay = this.$moment(currentDay).subtract(1, "days");
+          days.unshift(currentDay);
+        } while (currentDay.day() != MONDAY);
+      }
+
+      currentDay = this.$moment(days[days.length - 1]);
+
+      if (currentDay.day != SUNDAY) {
+        do {
+          currentDay = this.$moment(currentDay).add(1, "days");
+          days.push(currentDay);
+        } while (currentDay.day() != SUNDAY);
+      }
+
+      return days;
+    },
+    weeks() {
+      let weeks = [];
+      let week = [];
+
+      for (let day of this.days) {
+        week.push(day);
+        if (week.length == 7) {
+          weeks.push(week);
+          week = [];
+        }
+      }
+
+      return weeks;
+    }
+  }
+};
+</script>
